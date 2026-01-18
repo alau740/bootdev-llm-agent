77f31a56bb60e3bce094f805bb4005479164f583
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-18 20:27:29 +0000

    start filepath implementation

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,2 +1,7 @@
+import os
 def get_files_info(working_directory, directory="."):
+    file_path = os.path.abspath() # absolute path
+    target_dir = os.path.normpath(os.path.join(file_path, directory))
+    valid_path = os.path.commonpath([file_path, target_dir]) == working_directory
     pass
+
