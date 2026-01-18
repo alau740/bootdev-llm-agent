d0394bbd3c89936ce7ac635ca521acdef4c023bc
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-18 20:32:15 +0000

    add error handling

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,6 +2,8 @@ import os
 def get_files_info(working_directory, directory="."):
     working_dir_abs = os.path.abspath() # absolute path
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    valid_path = os.path.commonpath([working_dir_abs, target_dir]) == working_directory
-    pass
+    valid_path = os.path.commonpath([working_dir_abs, target_dir]) == working_directory # if target_dir falls in absolute dir
+    if not valid_path:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
 
