8f4d2e09f0f3f39aeb6796befe139d49a5d2ee1f
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-19 22:54:15 +0000

    Validation of directory paths

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -3,7 +3,7 @@ def get_files_info(working_directory, directory="."):
     working_dir_abs = os.path.abspath() # absolute path
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     valid_path = os.path.commonpath([working_dir_abs, target_dir]) == working_directory # if target_dir falls in absolute dir
-    if not valid_path:
+    if not os.path.isdir(directory):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
 
