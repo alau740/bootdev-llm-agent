85f1401e329891d4a74544e1da1e5b5da00a84c2
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-20 00:15:02 +0000

    fix error message

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -4,6 +4,6 @@ def get_files_info(working_directory, directory="."):
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
     valid_path = os.path.commonpath([working_dir_abs, target_dir]) == working_directory # if target_dir falls in absolute dir
     if not os.path.isdir(directory):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        return f'Error: Cannot list "{directory}" as it is not a directory'
     
 
