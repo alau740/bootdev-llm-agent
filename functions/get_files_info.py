99d0f5e0ee01346c3d30faf263d4a9f6abcc67cf
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-23 19:24:23 +0000

    implement file info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,12 +1,26 @@
 import os
 def get_files_info(working_directory, directory="."):
-    working_dir_abs = os.path.abspath() # absolute path
-    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    valid_path = os.path.commonpath([working_dir_abs, target_dir]) == working_directory # if target_dir falls in absolute dir
-    if not os.path.isdir(directory):
-        return f'Error: Cannot list "{directory}" as it is not a directory'
+    try:
+        working_dir_abs = os.path.abspath() # absolute path
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_path = os.path.commonpath([working_dir_abs, target_dir]) == working_directory # if target_dir falls in absolute dir
+        if not os.path.isdir(directory):
+            return f'Error: Cannot list "{directory}" as it is not a directory'
+        
+        # Check if target_dir is in working_dir_abs
+        valid_target_path = os.path.commonpath([working_dir_abs, target_dir])
+        if not valid_target_path:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+        files_info = []
+        for filename in os.listdir(target_dir):
+            filepath = os.path.join(target_dir, filename)
+            is_dir = os.path.isdir(filepath)
+            file_size = os.path.getsize(filepath)
+            files_info.append(
+                f"- {filename}: file_size={file_size} bytes, is_dir={is_dir}"
+            )
+        return "\n".join(files_info)
+    except Exception as e:
+        return f"Error listing files: {e}"
     
-    # Check if target_dir is in working_dir_abs
-    valid_target_path = os.path.commonpath([working_dir_abs, target_dir])
-    if not valid_target_path:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
