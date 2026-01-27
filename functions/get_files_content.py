a112ffacd61fd50669ae40b956cdc0dfc3a425bb
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-27 16:09:32 +0000

    File content

---
+++ functions/get_files_content.py
@@ -0,0 +1,7 @@
+import os
+def get_file_content(working_directory, file_path):
+        if os.path.commonpath([working_directory, file_path]) != working_directory:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(file_path): # not a file
+             return f'Error: File not found or is not a regular file: "{file_path}"'
+        
