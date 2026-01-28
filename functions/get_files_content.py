364deeac9744b231296aed43dfbc588c721fa422
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-28 01:13:52 +0000

    test

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -1,4 +1,5 @@
 import os
+# this is similar to get_files_info
 def get_file_content(working_directory, file_path):
         if os.path.commonpath([working_directory, file_path]) != working_directory:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
