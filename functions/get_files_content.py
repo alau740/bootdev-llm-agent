8f86a6b73df5523da189b74adef42fe6fa2e92ee
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-29 14:33:54 +0000

    test commit

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -3,6 +3,6 @@ import os
 def get_file_content(working_directory, file_path):
         if os.path.commonpath([working_directory, file_path]) != working_directory:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(file_path): # not a file
+        if not os.path.isfile(file_path): # not a file, safeguard
              return f'Error: File not found or is not a regular file: "{file_path}"'
         pass    
