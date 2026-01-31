88ddadd2be8526a308671300d6f3d20e396854a1
alau740 <77434904+alau740@users.noreply.github.com>
2026-01-31 16:44:10 +0000

    finish implementation of file content read

--- functions/get_files_content.py
+++ functions/get_files_content.py
@@ -2,20 +2,23 @@ import os
 MAX_CHARS = 10000 # character limit
 # this is similar to get_files_info
 def get_file_content(working_directory, file_path):
-        if os.path.commonpath([working_directory, file_path]) != working_directory:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        print(abs_working_dir, target_file)
+        if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
             return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-        if not os.path.isfile(file_path): # not a file, safeguard
-             return f'Error: File not found or is not a regular file: "{file_path}"'
-        
+        if not os.path.isfile(target_file): # not a file, safeguard
+            return f'Error: File not found or is not a regular file: "{file_path}"'
         try:
             # Open the file_path as read only, read up to MAX_CHARS
-            with open(file_path, "r") as f:
+            with open(target_file, "r") as f:
                 file_content_string = f.read(MAX_CHARS)
-            # After reading the first MAX_CHARS...
-            if f.read(1): # Read another character to see if there are any remaining
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-        except Exception:
-             return f"Error: {Exception}"
+                # After reading the first MAX_CHARS...
+                if f.read(1): # Read another character to see if there are any remaining
+                    file_content_string += (f'[...File "{file_path}" truncated at {MAX_CHARS} characters]')
+                    return file_content_string
+        except Exception as e:
+             return f"Error: {e}"
         
 
 
---
+++ test_get_file_content.py
@@ -0,0 +1,11 @@
+from functions.get_files_content import get_file_content
+
+def test():
+    result = get_file_content("calculator", "lorem.txt")
+    print("Result for current file:")
+    print(result)
+    print("")
+
+
+if __name__ == "__main__":
+    test()
