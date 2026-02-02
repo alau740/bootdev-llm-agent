5aadfc42219f1b10667450477d7cc69a639d446e
alau740 <77434904+alau740@users.noreply.github.com>
2026-02-02 00:28:43 +0000

    add unit testing of get files info

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 
 def test():
+    # Tests to ensure functionality
     result = get_files_info("calculator", ".")
     print("Result for current directory:")
     print(result)
