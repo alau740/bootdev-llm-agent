f49b9787353214da88acc898b225e77c8bd851a9
alau740 <77434904+alau740@users.noreply.github.com>
2026-02-04 00:36:16 +0000

    file name fix

--- test_write_file.py
+++ test_write_file.py
@@ -3,13 +3,13 @@ from functions.write_files_content import write_file
 def test():
     # Tests to ensure functionality
     result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    print(write_file)
+    print(result)
 
     result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    print(write_file)
+    print(result)
 
     result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(write_file)
+    print(result)
 
 
 if __name__ == "__main__":
