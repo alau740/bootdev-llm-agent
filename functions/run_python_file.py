185f8b3db0d2f3b7852ed8e1000dbeba033a04e2
alau740 <77434904+alau740@users.noreply.github.com>
2026-02-10 00:40:00 +0000

    Path validation

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -3,4 +3,16 @@ import os
 # Learning purposes only
 def run_python_file(working_directory, file_path, args=None):
     print(f"{working_directory}, {file_path}, {args}")
-    pass
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+        print(abs_working_dir, target_file)
+        if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(file_path): # Check to see if the path is a file
+            return f'Error: "{file_path}" does not exist or is not a regular file'
+        if not str.endswith('.py', [file_path]):
+            f'Error: "{file_path}" is not a Python file'
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+    
