f82d0020b37e68066c7d10a3965457a42ed8dbc3
alau740 <77434904+alau740@users.noreply.github.com>
2026-02-28 01:06:56 +0000

    Add schema

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from google.genai import types
 
 
 def run_python_file(working_directory, file_path, args=None):
@@ -34,3 +35,17 @@ def run_python_file(working_directory, file_path, args=None):
         return "\n".join(output)
     except Exception as e:
         return f"Error: executing Python file: {e}"
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs the specified python file, with a required parameter for the file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="Path to the python file to run, relative to the working directory (default is the working directory itself)",
+            ),
+        },
+    ),
+)
