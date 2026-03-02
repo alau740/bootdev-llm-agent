1ccd41d5727748f7cc5d4a37a3be6af0aae4b72a
alau740 <77434904+alau740@users.noreply.github.com>
2026-03-02 00:51:50 +0000

    Schema

--- functions/write_files_content.py
+++ functions/write_files_content.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 def write_file(working_directory, file_path, content):
     try:
         abs_working_dir = os.path.abspath(working_directory)
@@ -19,3 +20,22 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
     except Exception as e:
         return f"Error: {e}"
+    
+schema_write_files_content = types.FunctionDeclaration(
+    name="write_files_content",
+    description="Write the file content passed in as an argument, and writes this to the selected file",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="File path to write to, relative to the working directory",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="Text content to write to the file",
+            ),
+        },
+        required=["file_path", "content"],
+    ),
+)
