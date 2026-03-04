69bac2735142235107d03e409a9cacd598574824
alau740 <77434904+alau740@users.noreply.github.com>
2026-03-04 15:22:56 +0000

    schema

--- functions/call_function.py
+++ functions/call_function.py
@@ -4,6 +4,7 @@ from functions.get_files_info import schema_get_files_info
 from functions.run_python_file import schema_run_python_file
 from functions.write_files_content import schema_write_files_content
 from functions.get_files_content import schema_get_files_content
+# List of functions for LLM
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, 
                            schema_get_files_content,
