f41108c145a1bc3918b4ac051570a596346e20e9
alau740 <77434904+alau740@users.noreply.github.com>
2026-03-08 00:49:45 +0000

    Error handling

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google import genai
 from google.genai import types
 
 from functions.call_function import available_functions
+from functions.call_function import call_function
 from prompts import system_prompt
 
 
@@ -50,6 +51,14 @@ def generate_content(client, messages, verbose):
 
     for function_call in response.function_calls:
         print(f"Calling function: {function_call.name}({function_call.args})")
+        function_call_result = call_function(function_call.name(**function_call.args))
+        if function_call_result.parts is None:
+            raise TypeError("function_call_results has an empty parts list")
+        if function_call_result.parts[0].function_response is None:
+            raise TypeError("function_call_results is not FunctionResponse object")
+        if function_call_result.parts[0].function_response.response is None:
+            raise TypeError("function_call_results has no response")           
+
 
 
 if __name__ == "__main__":
