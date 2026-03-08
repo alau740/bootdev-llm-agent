7a7f4e294295f34f7771e9ef5c337fcae7f61c66
alau740 <77434904+alau740@users.noreply.github.com>
2026-03-08 00:33:56 +0000

    Function calling

--- functions/call_function.py
+++ functions/call_function.py
@@ -42,6 +42,20 @@ def call_function(function_call, verbose=False):
             ],
         )
     
+
+    
+    
     args = dict(function_call.args) if function_call.args else {}
     args["working_directory"] = "./calculator"
     
+    function_result = function_map[function_name](**args)
+
+    return types.Content(
+    role="tool",
+    parts=[
+        types.Part.from_function_response(
+            name=function_name,
+            response={"result": function_result},
+        )
+    ],
+)
