2fb6677b3fb6eaf208cc216b739aba5e65fdda89
alau740 <77434904+alau740@users.noreply.github.com>
2026-03-08 00:35:51 +0000

    cleanup

--- functions/call_function.py
+++ functions/call_function.py
@@ -42,9 +42,6 @@ def call_function(function_call, verbose=False):
             ],
         )
     
-
-    
-    
     args = dict(function_call.args) if function_call.args else {}
     args["working_directory"] = "./calculator"
     
