182dd4e8675586c3ab56b0fb45b3ff8eb113a1f2
alau740 <77434904+alau740@users.noreply.github.com>
2026-03-14 00:32:23 +0000

    Function call fix

--- functions/call_function.py
+++ functions/call_function.py
@@ -2,10 +2,10 @@ from google import genai
 from google.genai import types
 import functions
 from config import WORKING_DIR
-from functions.get_files_info import schema_get_files_info
-from functions.run_python_file import schema_run_python_file
-from functions.write_files_content import schema_write_files_content
-from functions.get_files_content import schema_get_file_content
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_files_content import schema_write_files_content, write_file
+from functions.get_files_content import schema_get_file_content, get_file_content
 # List of functions for LLM
 available_functions = types.Tool(
     function_declarations=[schema_get_files_info, 
@@ -16,17 +16,19 @@ available_functions = types.Tool(
 )
 
 function_map = {
-"get_file_content": functions.get_files_content,
-"get_file_info": functions.get_files_info,
-"write_files_content": functions.write_files_content.write_file,
-"run_python_file": functions.run_python_file.run_python_file
+"get_file_content": get_file_content,
+"get_files_info": get_files_info,
+"write_files_content": write_file,
+"run_python_file": run_python_file
 }
 
 def call_function(function_call, verbose=False):
-
-
-    function_name = function_call.name or ""
-
+    if verbose:
+        print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+        
+    function_name = function_call.name or ""    
     if function_name not in function_map:
         return types.Content(
             role="tool",
@@ -38,11 +40,6 @@ def call_function(function_call, verbose=False):
             ],
         )
 
-    if verbose:
-        print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(f" - Calling function: {function_call.name}")
-    
     args = dict(function_call.args) if function_call.args else {}
     args["working_directory"] = WORKING_DIR
     function_result = function_map[function_name](**args)
